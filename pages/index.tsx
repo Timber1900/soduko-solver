2fa1133572831a4bad900b83c5d4bb0b0ec91392
Timber1900 <Timber.gamer@gmail.com>
2021-12-28 20:26:11 +0100

    Preview (#1)
    * Fixed smartphone
    
    * Attempt to show only number keyboard in phone
    
    * Log debug info
    
    * Small stuff
    
    * Looks

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,43 +66,33 @@ export default function Home() {
       }
     }
 
-    //check if event.key is tab
     if (event.key === 'Tab') moveBy(1)
-    //check if event.key is backspace
     if (event.key === 'Backspace') moveBy(-1)
-    //cehck if event.key is up arrow
     if (event.key === 'ArrowUp') moveBy(-9)
-    //check if event.key is down arrow
     if (event.key === 'ArrowDown') moveBy(9)
-    //check if event.key is left arrow
     if (event.key === 'ArrowLeft') moveBy(-1)
-    //check if event.key is right arrow
     if (event.key === 'ArrowRight') moveBy(1)
-
-    console.log(event.key)
   }
 
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-300">
       <Head>
-        <title>Create Next App</title>
+        <title>Soduko Solver</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='h-4/5 aspect-square grid grid-cols-3 grid-rows-3 border_custom rounded-sm text-2xl bg-white'>
+      <div className='max-h-[80%] max-w-[80%] w-4/5 aspect-square grid grid-cols-3 grid-rows-3 border_custom rounded-sm text-2xl bg-white'>
         {shape.map((row, largeSquareIndex) => {
           return <div className='border border-black w-full h-full grid grid-cols-3 grid-rows-3' key={largeSquareIndex}>
             {row.map((val, smallSquareIndex) => {
-              //convert from large square index and small square index to row and col
               const row = Math.floor(largeSquareIndex / 3) * 3 + Math.floor(smallSquareIndex / 3)
               const col = (largeSquareIndex % 3) * 3 + (smallSquareIndex % 3)
-              //convert from row and col to index
               const index = row * 9 + col
 
-              return <div className={`${grid[index] ?? 0 == 0 ? "text-black": "text-gray-400"} border-[.5px] border-gray-400 w-full h-full grid place-content-center relative focus-within:text-3xl transition-all`} key={`col${smallSquareIndex}row${largeSquareIndex}`}>
-                <p id={`${index}`} className='focus:outline-none' contentEditable={edit} onKeyDown={onValChange} suppressContentEditableWarning={true}>{grid[index] ?? 0}</p>
-                <div className='absolute text-sm text-gray-500 right-0 bottom-0'>
-                  <p>
+              return <div className={`${grid[index] ?? 0 == 0 ? "text-black": "text-gray-400"} border-[.5px] border-gray-400 w-full h-full grid place-content-center relative focus-within:text-3xl md:text-2xl sm:text-lg text-base transition-all`} key={`col${smallSquareIndex}row${largeSquareIndex}`}>
+                <p id={`${index}`} className='focus:outline-none' inputMode='decimal' contentEditable={edit} onKeyDown={onValChange} suppressContentEditableWarning={true}>{grid[index] ?? 0}</p>
+                <div className='absolute md:text-sm text-xs text-gray-500 inset-0 flex flex-col items-end justify-end'>
+                  <p className='max-w-full truncate px-0.5'>
                     {gridPossible[index]?.reduce((acc, val) => acc ? `${acc}, ${val}` : `${val}`, "") ?? ""}
                   </p>
                 </div>
@@ -111,7 +101,7 @@ export default function Home() {
           </div>
         })}
       </div>
-      <div className='mt-12 flex flex-row gap-12 text-xl'>
+      <div className='mt-12 flex flex-row flex-wrap gap-12 text-xl items-center justify-center'>
         <button className='px-4 py-1 outline outline-black bg-white rounded-md' onClick={solveButton}>Solve</button>
         <button className='px-4 py-1 outline outline-black bg-white rounded-md' onClick={randomGrid}>Reset</button>
         <div className='flex flex-row justify-center items-center px-4 py-1 outline outline-black bg-white rounded-md'>
